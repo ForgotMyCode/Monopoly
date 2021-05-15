987f2f11a3e644dd9cceab7730ea2a3127d6470b
ForgotMyCode <nespamujtady@gmail.com>
2021-05-15 23:03:55 +0200

    We don't do spaces here

--- Monopoly/src/util/input.c
+++ Monopoly/src/util/input.c
@@ -10,151 +10,151 @@
 #include <config.h>
 
 void readInt(int* n) {
-    register int c;
-    int x = 0;
-    do {
+	register int c;
+	int x = 0;
+	do {
 #ifdef UNIX
-        c = getchar_unlocked();
+		c = getchar_unlocked();
 #else
-        c = getchar();
+		c = getchar();
 #endif
-    } while (c < '0' || c > '9');
-    do {
-        x = 10 * x + c - '0';
+	} while (c < '0' || c > '9');
+	do {
+		x = 10 * x + c - '0';
 #ifdef UNIX
-        c = getchar_unlocked();
+		c = getchar_unlocked();
 #else
-        c = getchar();
+		c = getchar();
 #endif
-    } while (!(c < '0' || c > '9'));
-    *n = x;
+	} while (!(c < '0' || c > '9'));
+	*n = x;
 }
 
 void freadLong(long* n, FILE* f) {
-    register int c;
-    long x = 0L;
-    do {
-        c = getc(f);
-    } while (c < '0' || c > '9');
-    do {
-        x = 10 * x + c - '0';
-        c = getc(f);
-    } while (!(c < '0' || c > '9'));
-    *n = x;
+	register int c;
+	long x = 0L;
+	do {
+		c = getc(f);
+	} while (c < '0' || c > '9');
+	do {
+		x = 10 * x + c - '0';
+		c = getc(f);
+	} while (!(c < '0' || c > '9'));
+	*n = x;
 }
 
 bool isStdChar(char c) {
-    return c >= ' ';
+	return c >= ' ';
 }
 
 ArrayList* readCSVLine(FILE* file) {
-    ArrayList* list = ArrayList_new(sizeof(char));
+	ArrayList* list = ArrayList_new(sizeof(char));
 
-    int read;
-    char c;
+	int read;
+	char c;
 
-    do {
-        read = getc(file);
-        c = (char)read;
-        ArrayList_add(list, &c);
-    } while (read != EOF && c != '\n');
+	do {
+		read = getc(file);
+		c = (char)read;
+		ArrayList_add(list, &c);
+	} while (read != EOF && c != '\n');
 
-    ((char*) list->buffer)[list->size - 1] = '\0';
+	((char*) list->buffer)[list->size - 1] = '\0';
 
-    return list;
+	return list;
 }
 
 ArrayList* parseCSV(char* filename) {
-    FILE* f = fopen(filename, "r");
-    assert(f);
+	FILE* f = fopen(filename, "r");
+	assert(f);
 
-    ArrayList* firstLine = readCSVLine(f);
-    ArrayList_delete(firstLine);
+	ArrayList* firstLine = readCSVLine(f);
+	ArrayList_delete(firstLine);
 
-    ArrayList* realties = ArrayList_new(sizeof(Realty*));
+	ArrayList* realties = ArrayList_new(sizeof(Realty*));
 
-    while (true) {
-        ArrayList* line = readCSVLine(f);
+	while (true) {
+		ArrayList* line = readCSVLine(f);
 
-        if (line->size <= 1) {
-            ArrayList_delete(line);
-            break;
-        }
+		if (line->size <= 1) {
+			ArrayList_delete(line);
+			break;
+		}
 
-        char* str = strdup((char*) line->buffer);
-        char* split;
-        char* strBuffer = str;
+		char* str = strdup((char*) line->buffer);
+		char* split;
+		char* strBuffer = str;
 
-        ArrayList_delete(line);
+		ArrayList_delete(line);
 
-        Realty* realty = Realty_new();
+		Realty* realty = Realty_new();
 
-        // name
-        split = strtok(str, ",");
-        assert(split);
+		// name
+		split = strtok(str, ",");
+		assert(split);
 
-        realty->name = strdup(split);
+		realty->name = strdup(split);
 
-        // price
-        split = strtok(NULL, ",");
-        assert(split);
+		// price
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->price = atol(split);
+		realty->price = atol(split);
 
-        // price per house
-        split = strtok(NULL, ",");
-        assert(split);
+		// price per house
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->pricePerHouse = atol(split);
+		realty->pricePerHouse = atol(split);
 
-        // rent
-        split = strtok(NULL, ",");
-        assert(split);
+		// rent
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->rent[0] = atol(split);
-        
-        // rent 1
-        split = strtok(NULL, ",");
-        assert(split);
+		realty->rent[0] = atol(split);
+		
+		// rent 1
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->rent[1] = atol(split);
+		realty->rent[1] = atol(split);
 
-        // rent 2
-        split = strtok(NULL, ",");
-        assert(split);
+		// rent 2
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->rent[2] = atol(split);
+		realty->rent[2] = atol(split);
 
-        // rent 3
-        split = strtok(NULL, ",");
-        assert(split);
+		// rent 3
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->rent[3] = atol(split);
+		realty->rent[3] = atol(split);
 
-        // rent 4
-        split = strtok(NULL, ",");
-        assert(split);
+		// rent 4
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->rent[4] = atol(split);
+		realty->rent[4] = atol(split);
 
-        // rent hotel
-        split = strtok(NULL, ",");
-        assert(split);
+		// rent hotel
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->rentHotel = atol(split);
+		realty->rentHotel = atol(split);
 
-        // mortgage
-        split = strtok(NULL, ",");
-        assert(split);
+		// mortgage
+		split = strtok(NULL, ",");
+		assert(split);
 
-        realty->mortgage = atol(split);
+		realty->mortgage = atol(split);
 
-        free(strBuffer);
+		free(strBuffer);
 
-        ArrayList_add(realties, &realty);
-    }
+		ArrayList_add(realties, &realty);
+	}
 
-    fclose(f);
+	fclose(f);
 
-    return realties;
+	return realties;
 }
